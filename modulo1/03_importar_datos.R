d7d5ecbb91614c785a4047bcc24014f97a2c7f4f
Paulo Villarroel <pvillarroeltapia@gmail.com>
2020-12-15 10:40:42 -0300

    Agrego carga desde repo

--- modulo1/03_importar_datos.R
+++ modulo1/03_importar_datos.R
@@ -1,45 +1,29 @@
 ## Academia OpenSAlud LAB
 ## Ciencia de Datos para el sectro público de salud
 
-## Encuesta nacional de salud 2017
 
 # Cargar librerías --------------------------------------------------------
-
+library(tidyverse)
 library(haven)
 
 # Cargar datos ------------------------------------------------------------
 
-ens <- read_sav("data/ENS_2017.sav")
+# Encuesta nacional de salud 2017
+# La encuesta se puede encontrar en http://epi.minsal.cl/bases-de-datos/
+ens <- read_sav("data/ENS_2017.sav") # SPSS
 View(ens)
 
-tabaquismo <- ens %>% 
-  select(1:11, starts_with("ta"))
-inicio_tab <- tabaquismo %>% 
-  select(ta6)
-inicio_tab$ta6 <- as_factor(inicio_tab$ta6)
-levels(inicio_tab$ta6)
-inicio_tab2 <- inicio_tab %>% 
-  filter(ta6 != "NO RESPONDE", ta6 != "NO SABE")
-inicio_tab2$ta6 <- as.numeric(inicio_tab2$ta6)
-inicio_tab2 %>% 
-  ggplot(aes(ta6)) +
-  geom_histogram(bins = 40) +
-  scale_x_continuous(breaks = seq(0, 70, by = 5))
+# Desde un repositorio
+covid <- read.csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto1/Covid-19_std.csv", encoding = "UTF-8")
 
-edad_tab <- inicio_tab2 %>% 
-  group_by(ta6) %>% 
-  summarise(n = n())
+# Desde Excel
 
 
-# Seleccion de variables curso --------------------------------------------
+# Desde un CSV
 
 
-encuesta <- ens %>% 
-  select(1, 3:5, 8:9, 11, 1088:1094, 1099:1101, 1106, 
-         starts_with(c("ta", "h", "di", "m2", "m4", "m5", "m8", "m9")))
 
-write.csv(encuesta, "data/encuesta.csv")
 
-df <- read.csv("data/encuesta.csv")
-
-colnames(encuesta)
+  
+  
+  
