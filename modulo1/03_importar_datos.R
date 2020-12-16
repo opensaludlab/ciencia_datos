d83cc03a62682b3e651b7181abe689d8af88b81d
Paulo Villarroel <pvillarroeltapia@gmail.com>
2020-12-16 08:43:04 -0300

    En desarrollo

--- modulo1/03_importar_datos.R
+++ modulo1/03_importar_datos.R
@@ -2,31 +2,51 @@
 ## Ciencia de Datos para el sectro público de salud
 ## Sesión importar datos
 
-#### ARCHIVO AUN EN DESARRLLO ####
+################################
+### ARCHIVO AUN EN DESARRLLO ###
+################################
 
-
-# Cargar librerías --------------------------------------------------------
 library(tidyverse)
+
+# Desde SPSS
 library(haven)
 
-# Cargar datos ------------------------------------------------------------
-
-# Encuesta nacional de salud 2017
-# La encuesta se puede encontrar en http://epi.minsal.cl/bases-de-datos/
-ens <- read_sav("data/ENS_2017.sav") # SPSS
+# Encuesta nacional de salud 2017 (http://epi.minsal.cl/bases-de-datos/)
+ens <- read_sav("data/ENS_2017.sav") 
 View(ens)
 
-# Desde un repositorio
+# Desde un repositorio en internet
 covid <- read.csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto1/Covid-19_std.csv", encoding = "UTF-8")
+View(covid)
 
 # Desde Excel
+library(readxl)
+
+poblacion_comunas <- read_excel("data/poblacion_comunas.xlsx")
+View(poblacion_comunas)
 
 
 # Desde un CSV
+# Es imporante fijarse en el tipo de separador
+casos <- read.csv("data/casos_covid.csv")
+
+# Desde datos espaciales
+# En http://www.ide.cl/ se pueden encontrar datos geográficos de Chile
+library(sf)
+library(chilemapas)
+
+help(package = "chilemapas")
+
+comunas_rm <- mapa_comunas[mapa_comunas$codigo_region == 13, ]
+ss_rm <- divisiones_salud %>% 
+  filter(str_detect(nombre_servicio_salud, "Metropolitano"))
+
+rm <- st_as_sf(comunas_rm) # Con esta función se puede trabajar como si fuera un data frame
+ggplot() + 
+  geom_sf(data = rm, aes(fill = codigo_comuna), show.legend = FALSE)
 
 
-# Desde ...
-
+# Guardar datos -----------------------------------------------------------
 
   
   
