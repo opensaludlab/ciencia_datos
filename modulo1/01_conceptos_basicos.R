1aafcc7428ea19a5751c4c1269bc632e7dbf694e
Paulo Villarroel <pvillarroeltapia@gmail.com>
2021-01-06 10:35:16 -0300

    Actualización carpeta

---
+++ modulo1/01_conceptos_basicos.R
@@ -0,0 +1,188 @@
+# Academia OpenSalud LAB Ciencia de Datos
+# Sesión inicial para revisar funcionalidades básicas de R y RStudio
+## Autor: Paulo Villarroel
+
+
+# Formas de escribir nombres de variables u objetos
+# Usa el que desees, pero se consistente con ello a lo largo del código.
+comunas.santiago
+comunasSantiago
+comunas_santiago_oriente
+
+# No iniciar con números o símbolos. Tampoco usar acentos ni espacios
+# RStudio te mostrará errores en la sintaxis
+03 <- "casa"
+tipo casa <- "chilena"
+%casas <- c(10, 20, 44)
+
+tipo_casa <- "chilena" # Este nombre si sirve
+
+# Cargar librerias --------------------------------------------------------
+
+# Las librerías solo se instalan 1 vez (por computador). Para eso se usa install.packages()
+install.packages("tidyverse")
+install.packages("knitr")
+# Para usar una librería, es necesario activarla usando la función library()
+library(tidyverse) # Esta librería la usaremos mucho!!!
+library(knitr)
+
+# Cargar data -------------------------------------------------------------
+
+data(iris) # iris es un dataset que viene incluído por defecto en R
+View(iris)
+
+# Aproximación inicial a los datos ----------------------------------------
+
+dim(iris) # n filas y columnas
+str(iris) # Ver la estructura de los datos
+names(iris) # Para ver los nombres de las variables
+head(iris, 3) # Puedes indicarle a head() el n° de observaciones ue quieres ver. Por defecto muestra las primeras 6
+tail(iris) # Con tail() puedes ver las últimas ovservaciones 
+summary(iris) # Análisis con estadígrafos básicos para variables numéricas
+glimpse(iris)
+min(iris$Sepal.Length) # seleccionar una variable con $
+max(iris$Sepal.Length)
+unique(iris$Species) # listar valores únicos
+table(iris$Species) # conteo de variables
+
+dim(Iris) # cuidado con las mayúsculas! 
+
+
+# Buscar ayuda ------------------------------------------------------------
+
+?glimpse
+example(colnames)
+vignette(all = FALSE)
+vignette(all = TRUE)
+vignette("grid")
+
+
+# Que se puede hacer ------------------------------------------------------
+
+
+## Lo siguiente es solo para que puedas ver algunas cosas que se pueden hacer en R. No te asustes.
+## La idea es que veas cómo funciona R y RStudio al guardar objetos, hacer visualizaciones o mostrar datos en consola
+## Todo lo veremos en más detalle en los módulos posteriores y con calma.
+
+
+# Visualizaciones básicas -------------------------------------------------
+
+# Ver algunas cosas de los datos (R base)
+
+plot(iris)
+hist(iris$Sepal.Length, 
+     main = "Histogram Sepal Length",
+     xlab = "Length (cms)",
+     col = "red") # Se puede cambiar el color 
+
+hist(iris$Sepal.Width, 
+     main = "Histogram Sepal Width",
+     xlab = "Width (cms)", 
+     col = "blue")
+
+# Usando uan de las librerías del tidyverse: dplyr
+# Fíjate en el uso del pipe %>%. Se puede leer así como "dado lo anterior, ahora haz lo siguiente"
+iris %>% 
+  group_by(Species) %>% 
+  summarise(avg = mean(Sepal.Length)) %>% 
+  arrange(avg)
+
+
+# Ver tipo de variables
+
+data_frame(variable = names(iris)) %>% # Al usar data_frame() da un Warning. Cámbialo por tibble()
+  mutate(class = map(iris, typeof)) %>% 
+  kable()
+
+
+# Formas de segmentar
+# Fíjate en el uso de == para indicar equivalencia.
+setosa <- iris %>% 
+  filter(Species == "setosa")
+setosa2 <- iris[iris$Species == "setosa", ] # Es lo mismo que lo anterior
+
+
+# Cambiar nombres de variables --------------------------------------------
+
+colnames(setosa)
+
+# FORMA 1
+colnames(setosa) <-  c("largo_sepalo", "ancho_sepalo", "largo_petalo", "ancho_petalo", "especie")
+
+# FORMA 2
+colnames(setosa)[colnames(setosa) == "largo_sepalo"] <- "Largo2"
+
+# FORMA 3
+names(setosa)[3] <- "Petalo2"
+
+
+# Gráficos con R base
+# Si bien no son tan lindos, son rápidos de generar y permiten obtener información de los datos
+plot(iris$Sepal.Length)
+plot(iris$Sepal.Length, iris$Sepal.Width)
+plot(iris$Sepal.Length, iris$Sepal.Width, col = iris$Species, pch = 19) # Puedes usar otros plot character (pch)
+
+plot(iris$Petal.Length, iris$Petal.Width,
+     col = iris$Species, 
+     pch = 17,
+     xlab = 'Longitud del pétalo', 
+     ylab = 'Ancho del pétalo')
+title(main = 'IRIS', 
+      sub = 'Exploración de los pétalos según especie',
+      col.main = 'blue', 
+      col.sub = 'blue')
+legend("bottomright", 
+       legend = levels(iris$Species),
+       col = unique(iris$Species), 
+       ncol = 3, 
+       pch = 19, 
+       bty = "n")
+
+boxplot(Petal.Length ~ Species, 
+        data = iris, 
+        notch = T)
+
+## Ahora con ggplot2
+# ggplot2 aporta mucha capacidad a las visualizaciones y permite gran personalización
+# En el módulo de visualización y análisis exploratorio veremos más detalles de los gráficos
+
+ggplot(iris, aes(x = Species, y = Sepal.Width)) +
+  geom_boxplot(notch = T, aes(fill = Species)) +
+  scale_fill_manual(name = "Especie", values = c('#a6cee3','#1f78b4','#b2df8a'))
+
+ggplot(iris, aes(x = Species, y = Sepal.Width)) +
+  geom_boxplot(notch = T, aes(fill = Species)) +
+  scale_fill_viridis_d(name = "Especie")
+
+ggplot(iris, aes(x = Species, y = Sepal.Width)) + 
+  geom_jitter(aes(shape = Species))
+
+ggplot(iris, aes(x = Species, y = Sepal.Width)) + 
+  geom_violin(fill = '#a6cee3') + 
+  coord_flip()
+
+# Se puede guardar en un objeto un gráfico
+p <- ggplot(iris, aes(x = Species, y = Sepal.Width)) + 
+  geom_boxplot() + 
+  geom_jitter(aes(color = Species)) # Cuidado con el orden de las capas. Intenta cambiando el orden de la línea 161 y 162
+p
+
+# Podríamos guardar algunas cosas
+
+# Plots
+# Una alternativa es usar la interfaz de RStudio "Export" o save image as
+# Pero también podemos hacerlo con código
+
+ggsave(p, filename = "boxplot_especies.png",
+       width = 20,
+       height = 15,
+       units = "cm")
+
+# Dataframe
+# Crear un dataframe
+edad <- c(2, 4, 67, 33, 2)
+ciudad <- c("Santiago", "Talca", "Puerto Montt", "Santiago", "Arica")
+df <- data.frame(edad, ciudad)
+
+write.csv(df, "ciudades.csv", row.names = FALSE) # Averigua qué sucede si no se usa el argumento row.names
+
---
+++ modulo1/02_tipos_datos.R
@@ -0,0 +1,196 @@
+## Academia OpenSalud LAB Ciencia de Datos
+## Sesión Tipos de datos, estructura y operadores
+## Autor: Paulo Villarroel
+
+
+install.packages("tidyverse") # Instala la librería si aún no lo has hecho
+library(tidyverse)
+
+# Tipo de datos -----------------------------------------------------------
+
+## Vectores
+
+x <- c(2, 4, 6, 8)
+y <- x + 8
+
+
+# Con la función class() se puede averiguar el tipo de dato
+class(x)
+edades <- c(13, 34, 24, 87, 9, 55, 45)
+edades
+
+data(uspop) # Datos de población de USA
+
+# Hacer subseting de un vector
+uspop[c(2, 5, 7)]
+x <- c(4, 7, 10, 13)
+uspop[x]
+
+
+## Dataframes
+
+data("iris")
+
+# Subseting de dataframe
+iris["Species"]
+iris$Species
+iris[, 3] # Indicar posición
+iris[, "Petal.Length"] # Indicar explícitamente el nombre de la variable
+iris[2:5, c(1, 5)] 
+iris$Petal.Length
+
+# Creemos un dataframe
+nombres <- c("Paulo", "María", "Diego", "Marcela", "Soledad")
+edades <- c(30, 22, 45, 19, NA)
+
+curso <- data.frame(nombres, edades)
+glimpse(curso)
+str(curso)
+
+
+## Una tabla de contigencia (untidy data)
+
+data("HairEyeColor")
+HairEyeColor
+HairEyeColor[,,1]
+HairEyeColor[,, "Male"] # Lo mismo que lo anterior, pero especificando el nombre de la tabla
+
+# Tranformar la tabla de contingencia a tidydata
+# Librería epitools y su función expand.table() sirve para pasar de un array a un data frame
+library(epitools)
+H <- expand.table(HairEyeColor)
+
+
+## Listas
+
+x <- list(rep("pepe", 3), 1:20)
+x[[1]]
+class(x[[1]])  # "character"
+length(x[[1]]) # 3
+
+x[[2]]
+class(x[[2]])  # "integer"
+length(x[[2]]) # 20
+
+# Las listas puedes incluir cualquier tipo de dato
+list_data <- list("Red", "Green", c(21, 32, 11), TRUE, 51.23, 119.1)
+
+# is.list function 
+a <- list(1, 2, 3)
+b <- list(c("Jan", "Feb", "Mar")) 
+c <- list(matrix(c(1, 2, 3, 4, 5))) 
+d <- list(list("green", 12.3)) 
+
+# Usando la función is.list()
+is.list(a)
+is.list(b) 
+is.list(c)
+is.list(d) 
+
+merged_list <- c(a, b, c, d) 
+
+
+# Coerción ----------------------------------------------------------------
+
+# Esta sección la dejo para que puedas revisarla más tarde con la documentación que estará disponible.
+# Te será útil para los módulos de limpieza de datos, transformación y analisis exploratorio.
+
+# Asignar tipo de dato con función as()
+as.character(5)
+as.logical(TRUE)
+as.numeric("siete")
+
+# Averiguar tipo de dato
+class(iris)
+class("academia")
+is.logical(4)
+is.character(4)
+
+# Veamos el caso especial de los factores
+as.factor(5)
+as.factor("cinco")
+factor_cinco <- as.factor("cinco")
+
+# Coercionar de factor a character
+as.character(factor_cinco)
+as.numeric(factor_cinco)
+
+# Cambio de tipo de datos
+puntos <- c("North", "South", "East", "East") 
+puntos
+puntos2 <- factor(puntos)
+puntos2
+as.numeric(puntos2) # Los ordena alfabeticamente
+
+# Creando un factor
+metros <- factor(c(29, 28, 210, 28, 29))
+levels(metros)
+
+# Convertir factor a numeric
+as.numeric(metros) #Mira la diferencia
+
+metros2 <- as.numeric(as.character(metros))
+str(metros2)
+
+# Crear un vector
+x <- c("female", "male", "male", "female") 
+
+# Usar as.factor() para convertir vector a un factor 
+as.factor(x)
+
+
+# Operadores --------------------------------------------------------------
+
+# Aritméticos
+5 * 3
+6 + 4
+8 - 5
+7 / 3
+5 %% 3 # Devuelve el residuo
+4 %% 2
+3 ^ 2 # Potencia (elevado a...)
+4 + "tres"
+3 * NA # Fíjate qué pasa con los NA
+7 + NA
+
+# Relacionales
+5 > 4
+4 == 4
+"casa" != "perro"
+"casa" > "auto" # Compara la posición alfabética de las letras
+
+# Lógicos
+# Todos los valores numéricos mayores a 0 son coercionados a TRUE, 0 es coercionado a FALSE
+5 | 3
+0 & 3
+5 | 0
+!(FALSE | FALSE)
+
+# Orden de los operadores
+5 * 3 + (4 ^ 2) + (FALSE | TRUE) # Prueba a hacer los tuyos!!
+class(TRUE)
+as.numeric(TRUE)
+
+
+# Tidy data ---------------------------------------------------------------
+
+# Tarea para la casa
+# Transformar a tidydata con pivot_longer() de la librería tidyr
+# Puedes ver info en https://tidyr.tidyverse.org/ 
+
+iris %>% 
+  pivot_longer(cols = -Species, 
+                      names_to = "Type",
+                      values_to = "value")
+
+# Separar campos al pivotear
+# Acá usaremos una expresión regular (regex), pero solo para que veas las posibilidades que ofrece
+# Las expresiones regulares no son tan fáciles de entender, pero abren un espacio gigante de posibilidades
+
+iris %>% 
+  pivot_longer(cols = -Species, 
+                      names_to = c("part", "dimension"),
+                      names_pattern = "(.*)\\.(.*)", # Expresión regular
+                      values_to = "value")
+
+# Cheat sheet regex https://rstudio.com/wp-content/uploads/2016/09/RegExCheatsheet.pdf
