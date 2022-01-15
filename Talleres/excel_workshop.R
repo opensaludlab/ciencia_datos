f664a3a2e2ee98a1e9f1c3fc8bc4e74035266daf
Paulo Villarroel <pvillarroeltapia@gmail.com>
2022-01-15 00:27:49 -0300

    Upload workshop script

---
+++ Talleres/excel_workshop.R
@@ -0,0 +1,66 @@
+library(readxl)
+library(writexl)
+library(tidyverse)
+
+indicators <- read_excel("Talleres/data/Popular_Indicators.xlsx", sheet = "Data")
+
+str(indicators)
+colnames(indicators)
+unique(indicators$`serie Name`)
+tail(indicators)
+
+indicators <- read_excel("Talleres/data/Popular_Indicators.xlsx", 
+                         sheet = "Data",
+                         range = "A1:Y11068")
+
+
+# Leer multiples archivos
+
+files <- list.files(path = "Talleres/data", pattern = "archivo", full.names = TRUE) |> 
+  map_df(~ read_excel(., col_names = TRUE))
+
+
+
+# Leer archivos con varias hojas
+
+sheet_names <- excel_sheets("Talleres/data/archivo1.xlsx")
+sheet_names 
+
+list_all <- lapply(sheet_names, function(x) {
+  as.data.frame(read_excel("Talleres/data/archivo1.xlsx", sheet = x, col_names = TRUE))
+})
+
+names(list_all) <- sheet_names
+list_all
+
+
+# Varios archivos, varias hojas
+
+library(data.table)
+
+files_path <- list.files(path = "Talleres/data", pattern = "archivo", full.names = TRUE)
+
+names_files <- list.files(path = "Talleres/data", pattern = "archivo") |>
+  str_extract_all(pattern = ".*[^.xlsx]", simplify = TRUE)
+
+
+df_list <- lapply(files_path, function(x) {
+  sheets <- excel_sheets(x)
+  dfs <- lapply(sheets, function(y) {
+    read_excel(x, sheet = y, col_names = TRUE)
+  })
+  names(dfs) <- sheets
+  dfs
+})
+
+names(df_list) <- names_files
+
+# Pasar de una lista a dataframe
+data <- rbindlist(lapply(df_list, rbindlist, id = "mes"))
+
+
+
+# Guardar Excel
+
+write_xlsx(data, "Talleres/data/data_completa.xlsx")
+
