bb3d2495fc30f8127edad854d1d4b94ee69e8a81
paulovillarroel <pvillarroeltapia@gmail.com>
2022-03-19 22:04:46 -0300

    Add first code challenge

---
+++ desafios-programacion/00-palindrome.R
@@ -0,0 +1,39 @@
+## palindrome
+
+# Crea un programa que permita identificar si una palabra es un palíndromo.
+# Un palíndormo es una palabra que se lee igual de izquierda a derecha que de derecha a izquierda.
+
+
+library(tidyverse)
+
+palindrome <- function(x) {
+  if (is.character(x) == FALSE) {
+    "No es una palabra"
+  } else {
+    x <- tolower(x) |> 
+       str_trim()
+    split <- strsplit(x, "")[[1]]
+    reversed <- rev(split)
+    join <- paste(reversed, collapse = "")
+
+    if (join == x) {
+      "Es un palíndromo"
+    } else {
+      "No es un palíndromo"
+    }
+  }
+}
+
+palindrome("paulo")
+palindrome("radar ")
+palindrome("ana")
+palindrome("auto")
+palindrome(44444)
+
+
+
+
+
+
+
+
