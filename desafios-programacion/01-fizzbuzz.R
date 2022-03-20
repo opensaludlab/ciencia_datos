3ddaf1591d3a5de616ee6bfd304d8466684628c5
paulovillarroel <pvillarroeltapia@gmail.com>
2022-03-20 19:14:32 -0300

    Add second challenge

---
+++ desafios-programacion/01-fizzbuzz.R
@@ -0,0 +1,22 @@
+## Desafío de programación 01 FizzBuzz -----
+#
+#
+# Escribe un programa que muestre por consola (con un print) los números de 1 a 100 (ambos incluidos), sustituyendo los siguientes:
+# - Múltiplos de 3 por la palabra "Fizz".
+# - Múltiplos de 5 por la palabra "Buzz".
+# - Múltiplos de 3 y de 5 a la vez por la palabra "FizzBuzz".
+#
+#
+# Este problema es muy conocido en el mundo de la programación. Si lo buscas en Google tendrán cientos de respuestas e muchos lenguajes.
+# Si bien puedes hacer eso, te aconsejo que en primera instancia trates de hacerlo solo/a. No copies y pegues código.
+# Así no aprenderás a desarrollar tu lógica. 
+# Hay diversas formas de resolver este desafío. Es más simple y clásico es usando un bucle for().
+# Puedes leer sobre ello en https://es.r4ds.hadley.nz/iteraci%C3%B3n.html#introducci%C3%B3n-14
+#
+#
+# Como es el primer desafío, te dejo este video como ayuda para comprender el problema y orientar tu respuesta.
+# Pero como mencioné antes, intenta hacerlo tu primero. No importa que no te resulte, lo relevante es intentar y practicar.
+# https://youtu.be/QPZ0pIK_wsc
+#
+# Recuerda que tienes el servidor de Discord del Bootcamp para que puedas hacer tu preguntas y resolver dudas.
+
